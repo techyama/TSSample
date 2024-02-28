80b8a4e68ffd8bbca82d6f746b388f48148f4e2b
i-yamamotopro <yamamoto.ikki.tech@gmail.com>
2024-02-28 19:26:51 +0900

    コンフリクト解消

--- src/oop/shogi.ts
+++ src/oop/shogi.ts
@@ -4,62 +4,62 @@ type Player = 'first' | 'second'
 
 // 駒の位置を表すクラス
 class Position {
-    constructor(
-        private suji: Suji,
-        private dan: Dan,
-    ) { }
+  constructor(
+    private suji: Suji,
+    private dan: Dan,
+  ) {}
 
-    // パラメーターに渡された位置と現在位置を比較するメソッド
-    distanceFrom(position: Position, player: Player) {
-        if (player === 'first') {
-            return {
-                suji: Math.abs(position.suji - this.suji),
-                dan: Math.abs(Number(position.dan) - Number(this.dan)),
-            }
-        } else {
-            return {
-                suji: Math.abs(position.suji - this.suji),
-                dan: -Math.abs(Number(position.dan) - Number(this.dan)), // 団(縦の位置)は正負反転
-            }
-        }
+  // パラメーターに渡された位置と現在位置を比較するメソッド
+  distanceFrom(position: Position, player: Player) {
+    if (player === 'first') {
+      return {
+        suji: Math.abs(position.suji - this.suji),
+        dan: Math.abs(Number(position.dan) - Number(this.dan)),
+      }
+    } else {
+      return {
+        suji: Math.abs(position.suji - this.suji),
+        dan: -Math.abs(Number(position.dan) - Number(this.dan)), // 団(縦の位置)は正負反転
+      }
     }
+  }
 }
 
 // 駒を表すクラス
 abstract class Piece {
-    // Pieceクラスとサブクラスでアクセスできる
-    protected position: Position
+  // Pieceクラスとサブクラスでアクセスできる
+  protected position: Position
 
-    constructor(
-        private readonly player: Player,
-        suji: Suji,
-        dan: Dan,
-    ) {
-        this.position = new Position(suji, dan)
-    }
+  constructor(
+    private readonly player: Player,
+    suji: Suji,
+    dan: Dan,
+  ) {
+    this.position = new Position(suji, dan)
+  }
 
-    // メソッドの定義
-    moveTo(position: Position) {
-        this.position = position
-    }
+  // メソッドの定義
+  moveTo(position: Position) {
+    this.position = position
+  }
 
-    // 移動できるかどうかを判定するメソッド
-    abstract canMoveTo(position: Position, player: Player): boolean
+  // 移動できるかどうかを判定するメソッド
+  abstract canMoveTo(position: Position, player: Player): boolean
 }
 
 class Osho extends Piece {
-    // 王将のcanMoveToメソッドを具体的に実装する
-    canMoveTo(position: Position, player: Player): boolean {
-        const distance = this.position.distanceFrom(position, player)
-        return distance.suji < 2 && distance.dan < 2
-    }
+  // 王将のcanMoveToメソッドを具体的に実装する
+  canMoveTo(position: Position, player: Player): boolean {
+    const distance = this.position.distanceFrom(position, player)
+    return distance.suji < 2 && distance.dan < 2
+  }
 }
 
 class Game {
-    private pieces = Game.makePieces()
-    private static makePieces() {
-        return [new Osho('first', 5, '1'), new Osho('second', 5, '9')]
-    }
+  private pieces = Game.makePieces()
+  private static makePieces() {
+    return [new Osho('first', 5, '1'), new Osho('second', 5, '9')]
+  }
 }
 
 new Game()
